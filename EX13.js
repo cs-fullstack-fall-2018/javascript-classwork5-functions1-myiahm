53524cf0ae053418dacb734e126031dc1132808d
Myiah Moore <myiahsade@gmail.com>
2018-07-31 11:49:05 -0500

    check in

---
+++ EX13.js
@@ -0,0 +1,21 @@
+main();
+function alertKevin(favoriteTeacher)
+{
+alert(favoriteTeacher)
+}
+function printHello()
+{
+    alert("Hello")
+}
+function printWorld()
+{
+ alert("World!!!!")
+}
+function main()
+{
+    var favoriteTeacher = "Kevin";
+
+    printHello();
+    printWorld();
+    alertKevin(favoriteTeacher);
+}
